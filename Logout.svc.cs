07316b74b472b372384f78a3314ff054830464a6
ThandoMchunu <148089960+ThandoMchunu@users.noreply.github.com>
2024-08-17 15:49:35 +0200

    Update Logout.svc.cs

--- Logout.svc.cs
+++ Logout.svc.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Web;
 
 namespace backend1
 {
@@ -11,58 +13,46 @@ namespace backend1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Logout.svc or Logout.svc.cs at the Solution Explorer and start debugging.
     public class Logout : ILogout
     {
-        DataClasses1DataContext db = new DataClasses1DataContext();
-        private readonly ILogger<Login> logger;
-        public Login(ILogger<Login> logger)
+        private readonly ILogger<Logout> logger;
+        string ILogout.Logout(HttpContext context)
         {
-            this.logger = logger;
-        }
-        bool ILogin.Login(HttpContext context, string email, string password)
-        {
-            bool isAuthenticated = false;
-            HttpResponse response = HttpContext.Current.Response;
-
             try
             {
+                //Clear the user's sessions
+                context.Session.Clear();  //Clear the user's sessions
+                context.Session.Abandon();  //Cancel the current session and release resources. (also generates a new ID for the next session)
 
-                //fetch user infor from db and compare
-                var u = (from dbUser in db.Users
-                         where dbUser.Email.Equals(email) && dbUser.Password_Hash == Secrecy.HashPassword(password)
-                         select dbUser).FirstOrDefault();
-
-
-                //check if user exists
-                if (u != null)
+                //Clear the client's cookie variables
+                if (context.Request.Cookies[".ASPXAUTH"] != null)    //Check if the authentication cookie variable exists
                 {
-                    //get the user type
-                    bool role = u.User_Type;
-
-                    if (role)
-                    {
-                        //redirect user to Admin page if role is true
-                        response.Redirect("~/admin.aspx");
-                    }
-
-                }
-                else
-                {
-
-                    //redirect user to Home page if role is true
-                    response.Redirect("~/Home.aspx");
+                    //create a cookie instance with an empty value
+                    var cookie = new HttpCookie(".ASPXAUTH", string.Empty);
+                    //delete the cookie
+                    cookie.Expires = DateTime.Now.AddDays(-1);  //set the cookies expiration date to a past data to delete it
+                                                                // add cookie to the response command of http to remove the cookie from the client-side
+                    context.Response.Cookies.Add(cookie);
                 }
 
-                isAuthenticated = true;
+                //Redirect the user to the landing page after successful log out
+                context.Response.Redirect("~/landing-page.aspx");
+
+                //Success message sent
+                string logoutMssg = "Successfully Logged Out";
+                context.Response.Write(logoutMssg);
+                return logoutMssg;
 
             }
             catch (Exception ex)
             {
-                //Add a error meassage log
-                this.logger.LogError(ex, "Error occured during login for user with email: ", email);
+                //Error handling
+                this.logger.LogError(ex, "Error occured during login for user with email: ");
+                string failMssg = "Error logging out. Try again";
+                context.Response.Write(failMssg);
+                return failMssg + ":" + ex.Message;
 
-                isAuthenticated = false;
-                return isAuthenticated;
             }
-            return isAuthenticated;
+
         }
     }
 }
+
