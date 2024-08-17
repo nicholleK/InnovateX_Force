a523af392540cc0cd09bcd37eef8f80f044ed4ee
ThandoMchunu <148089960+ThandoMchunu@users.noreply.github.com>
2024-08-17 15:47:58 +0200

    Update Logout.svc.cs

--- Logout.svc.cs
+++ Logout.svc.cs
@@ -11,8 +11,58 @@ namespace backend1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Logout.svc or Logout.svc.cs at the Solution Explorer and start debugging.
     public class Logout : ILogout
     {
-        public void DoWork()
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        private readonly ILogger<Login> logger;
+        public Login(ILogger<Login> logger)
         {
+            this.logger = logger;
+        }
+        bool ILogin.Login(HttpContext context, string email, string password)
+        {
+            bool isAuthenticated = false;
+            HttpResponse response = HttpContext.Current.Response;
+
+            try
+            {
+
+                //fetch user infor from db and compare
+                var u = (from dbUser in db.Users
+                         where dbUser.Email.Equals(email) && dbUser.Password_Hash == Secrecy.HashPassword(password)
+                         select dbUser).FirstOrDefault();
+
+
+                //check if user exists
+                if (u != null)
+                {
+                    //get the user type
+                    bool role = u.User_Type;
+
+                    if (role)
+                    {
+                        //redirect user to Admin page if role is true
+                        response.Redirect("~/admin.aspx");
+                    }
+
+                }
+                else
+                {
+
+                    //redirect user to Home page if role is true
+                    response.Redirect("~/Home.aspx");
+                }
+
+                isAuthenticated = true;
+
+            }
+            catch (Exception ex)
+            {
+                //Add a error meassage log
+                this.logger.LogError(ex, "Error occured during login for user with email: ", email);
+
+                isAuthenticated = false;
+                return isAuthenticated;
+            }
+            return isAuthenticated;
         }
     }
 }
